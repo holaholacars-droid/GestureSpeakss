e08891536aab0060aa7e7414f4afc0b85f154020
charles65911-arch <charles65911@gmail.com>
2025-10-03 18:28:52 +0530

    Update manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():hh
-    """Run administrative tasks."""
+    """Run administrative questions."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
         from django.core.management import execute_from_command_line
